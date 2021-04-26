3519f9c1a2bc44ccd35436679f8ccb67d8f78906
Leon <33227102+Toxillo@users.noreply.github.com>
2021-04-26 18:00:56 +0200

    test

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const http = require('http').createServer();
+
+const io = require('socket.io')(http, {
+    cors: { orgin: "*"}
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('message', (message) => {
+        console.log(message);
+        io.emit('message', 'Hello World');
+    });
+});
+
+http.listen(8080, () => console.log('listening on http://localhost:8080'));
+
+
